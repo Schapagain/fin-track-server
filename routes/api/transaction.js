bcf27498800bd07236cda9ec99aa5c81d9701c2a
Sandesh C <sandesh@greatbear.tech>
2020-10-16 21:11:26 -0400

    add /add and /view routes to transaction

--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
-router.post('/', (req,res) => {
+router.post('/add', (req,res) => {
     console.log('Posting to transactions');
 })
 
-router.get('/', (req,res) => {
+router.get('/view', (req,res) => {
     console.log('Getting from transactions');
+    res.status(200).json({
+        message: 'You have all transactions now'
+    })
 })
 
 module.exports = router;
