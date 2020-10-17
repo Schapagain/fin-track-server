3de913cce3ac0063b6696b1f1bf4b830e494a4f2
Sandesh C <sandesh@greatbear.tech>
2020-10-16 21:11:40 -0400

    add morgan to track http requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+// Log request before processing them
+app.use(morgan('dev'));
 
 // Body parser
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const transactionRoutes = require('./routes/api/transaction');
