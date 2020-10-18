37de0cdec677a487112815712eb078e62ea40f2d
Sandesh C <sandesh@greatbear.tech>
2020-10-18 14:13:34 -0400

    post newly added transactions to the backend

--- client/src/actions/transactionActions.js
+++ client/src/actions/transactionActions.js
@@ -4,7 +4,7 @@ import { GET_TRANSACTIONS, ADD_TRANSACTION, TRANSACTIONS_LOADING} from './types'
 export const getTransactions = () => async dispatch => {
     dispatch(setTransactionsLoading());
     try{
-        const result = await axios.get('http://localhost:5000/api/transactions')
+        const result = await axios.get('http://localhost:5000/api/transactions');
         dispatch({
             type: GET_TRANSACTIONS,
             payload: result.data.transactions
@@ -15,11 +15,19 @@ export const getTransactions = () => async dispatch => {
     }
 };
 
-export const addTransaction = transaction => {
-    return {
-        type: ADD_TRANSACTION,
-        payload: transaction
-    };
+export const addTransaction = transaction => async dispatch => {
+    
+    try{
+        const result = await axios.post('http://localhost:5000/api/transactions',transaction);
+        dispatch({
+            type: ADD_TRANSACTION,
+            payload: result.data.transaction
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+
 };
 
 export const setTransactionsLoading = () => {
