a07e4bdb753d8e8cc9c78bd7703664357145dc9a
Sandesh C <sandesh@greatbear.tech>
2020-10-18 15:41:39 -0400

    get script ready for heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const transactionRoutes = require('./routes/api/transactions');
+const path = require('path');
 
 // Log request before processing them
 app.use(morgan('dev'));
@@ -23,6 +24,15 @@ app.use((req,res,next) => {
     next();
 });
 
+// Serve API routes
 app.use('/api/transactions',transactionRoutes);
+
+// Serve static content in production
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
 
 module.exports = app;
