decb6c562b35d1db5f92cb58281e2a45f05508f5
Sandesh C <sandesh@greatbear.tech>
2020-10-18 14:12:47 -0400

    enable CORS

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Enable Cross Origin Sharing for everyone
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','*');
+
+    // Handle initial OPTIONS request
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/api/transactions',transactionRoutes);
 
 module.exports = app;
