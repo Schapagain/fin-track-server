212db67a0e31f6e6968f918cdf5205db78d4ce2d
Sandesh C <sandesh@greatbear.tech>
2020-10-19 13:52:47 -0400

    add utility function to compute cumulative transactions for the current month

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -7,3 +7,32 @@ export const getPrettyDate = transaction =>{
         date: moment(transaction.date).format('MMM Do YY')
     }
 }
+
+export const getCumulativeAmountsForCurrentMonth = transactions => {
+
+    // Filter transactions for this month
+    // and sort them in ascending order of date
+    // Note: API returns in descending order
+    transactions = 
+    transactions
+        .filter( transaction => moment(transaction.dateObj).month() === moment().month())
+        .map( transaction => ({day: moment(transaction.dateObj).date(), type: transaction.type, amount: transaction.amount, dateObj: transaction.dateObj}))
+        .reverse()
+    
+    // Sum up transactions for each day where at least one transaction has occured
+    let amounts = new Map();
+    for (let transaction of transactions){
+        const currentAmount = _getSignedAmount(transaction);
+        const currentDay = moment(transaction.dateObj).format('Do');
+        amounts.has(currentDay)? amounts.set(currentDay,amounts.get(currentDay) + currentAmount) : amounts.set(currentDay,currentAmount);
+    }
+
+    // Find cumulative amounts for all such days
+    const cumulativeAmounts = [...amounts.values()].map((sum => value => sum += value)(0))
+    return { days: [...amounts.keys()], amounts: cumulativeAmounts}
+}
+
+const _getSignedAmount = transaction => {
+    if (transaction.type === 'expense') return -1 * transaction.amount;
+    return transaction.amount;
+}
