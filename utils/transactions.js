0ac7378f3b728a56ad633ce33766d75e4fe64c0f
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-24 14:07:48 -0400

    include category in response

--- utils/transactions.js
+++ utils/transactions.js
@@ -4,8 +4,8 @@ const getCleanTransactions = transactions => {
     if (!Array.isArray(transactions)) transactions = [transactions]
 
     return transactions.map( transaction => {
-        let {id,amount,title,date,type} = transaction;
-            return {id,amount,title,date,type}
+        let {id,amount,title,date,type,category} = transaction;
+            return {id,amount,title,date,type,category}
     })
 }
 
