8661a8f18ab30bb0e9a6e660e0979be8cbeab257
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-23 20:24:16 -0400

    remove dev middleware

--- client/src/store.js
+++ client/src/store.js
@@ -5,8 +5,7 @@ import rootReducer from './reducers';
 const initState = {};
 const middlewares = [thunk];
 const store = createStore(rootReducer, initState, compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middlewares)
 ));
 
 export default store;
