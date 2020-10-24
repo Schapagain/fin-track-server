57a50b1d89fb3e0fd3d933780d29c75877a68cf1
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-24 14:09:41 -0400

    add utility functions to display filtered transactions

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,3 @@
-
 const moment = require('moment');
 export const getPrettyDate = transaction =>{
     return {
@@ -9,18 +8,10 @@ export const getPrettyDate = transaction =>{
 }
 
 export const getCumulativeAmounts = transactions => {
-
-    // Filter transactions for this month
-    // and sort them in ascending order of date
-    // Note: API returns in descending order
-    transactions = 
-    transactions
-        .map( transaction => ({type: transaction.type, amount: transaction.amount, dateObj: transaction.dateObj}))
-        .reverse()
     
     // Sum up transactions for each day where at least one transaction has occured
     let amounts = new Map();
-    for (let transaction of transactions){
+    for (let transaction of [...transactions].reverse()){
         const currentAmount = _getSignedAmount(transaction);
         const currentDay = moment.utc(transaction.dateObj).format('MMM Do');
         amounts.has(currentDay)? amounts.set(currentDay,amounts.get(currentDay) + currentAmount) : amounts.set(currentDay,currentAmount);
@@ -28,7 +19,41 @@ export const getCumulativeAmounts = transactions => {
 
     // Find cumulative amounts for all such days
     const cumulativeAmounts = [...amounts.values()].map((sum => value => sum += value)(0))
-    return { days: [...amounts.keys()], amounts: cumulativeAmounts, month: moment.utc().format('MMMM')}
+    return { xvalues: [...amounts.keys()], yvalues: cumulativeAmounts }
+}
+
+export const getCategoricalAmounts = transactions => {
+
+    let amounts = new Map();
+    for (let transaction of [...transactions].reverse()){
+        const currentAmount = transaction.amount;
+        const currentCategory = transaction.category;
+        amounts.has(currentCategory)? amounts.set(currentCategory,amounts.get(currentCategory) + currentAmount) : amounts.set(currentCategory,currentAmount);
+    }
+    return { xvalues: [...amounts.keys()], yvalues: [...amounts.values()]}
+}
+
+export const getMonthlyCategoricalAmounts = transactions => {
+    // Sum up transactions for each day where at least one transaction has occured
+    let amounts = new Map();
+    for (let transaction of [...transactions].reverse()){
+        const currentAmount = transaction.amount;
+        const currentMonth = moment.utc(transaction.dateObj).format('MMM YY');
+        amounts.has(currentMonth)? amounts.set(currentMonth,amounts.get(currentMonth) + currentAmount) : amounts.set(currentMonth,currentAmount);
+    }
+
+    return { xvalues: [...amounts.keys()], yvalues: [...amounts.values()] }
+}
+
+export const getTitleCase = text => {
+    return text.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+}
+
+export const getCurrentFilter = (type,category) => {
+    type = type? (type === 'income'? "All Income":"All Expenses"):"";
+    type = getTitleCase(type);
+    category = getTitleCase(category);
+    return category? category:(type? type:"All transactions");
 }
 
 const _getSignedAmount = transaction => {
