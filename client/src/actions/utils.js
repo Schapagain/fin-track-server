a07a465dc7cd8292751051f8d04e9e4502b0cba1
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-11 07:22:19 -0500

    fix export

--- client/src/actions/utils.js
+++ client/src/actions/utils.js
@@ -1,9 +1,5 @@
 
 
-const getServerAddress = () => {
+export const getServerAddress = () => {
     return process.env.NODE_ENV === "production"? "https://my-ftrack-app.herokuapp.com/":"http://localhost:5000";
-}
-
-export default {
-    getServerAddress,
 }
