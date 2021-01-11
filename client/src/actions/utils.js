28e6cc79ae109658b899697ee96e5a0269d91577
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-11 07:25:16 -0500

    fix server address

--- client/src/actions/utils.js
+++ client/src/actions/utils.js
@@ -1,5 +1,5 @@
 
 
 export const getServerAddress = () => {
-    return process.env.NODE_ENV === "production"? "https://my-ftrack-app.herokuapp.com/":"http://localhost:5000";
+    return process.env.NODE_ENV === "production"? "https://my-ftrack-app.herokuapp.com":"http://localhost:5000";
 }
